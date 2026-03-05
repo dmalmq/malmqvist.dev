4678b018dd2e7c29c7e42afbea581da11b1d9265
Daniel Malmquist <daniel@example.com>
2026-03-06 08:50:19 +0900

    Configure Vercel + WebAnalytics Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,17 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://malmqvist.dev',
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
